eb358d491ce7729847a6cc4f0b63f60da7efc177
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2022-11-19 12:55:29 -0300

    scripts para lava da torre

---
+++ Assets/Scripts/Lava/LavaTriggerTower.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTriggerTower : MonoBehaviour
+{
+    public string compareTag = "Player";
+
+    public Animator animatorLava;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if (other.transform.CompareTag(compareTag))
+        {
+            animatorLava.SetTrigger("Lava");
+        }*/
+
+        animatorLava.SetTrigger("Lava");
+    }
+}
